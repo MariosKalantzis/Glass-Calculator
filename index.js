50f664ba73e97610bd74c54ae6c64ebe1ceea654
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-22 21:07:52 +0300

    Add error handling for division by zero and improve clear functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const operations = {
     "*": (a, b) => a * b,
     "/": (a, b) => a / b
 };
-
+let isAfterOperate = false;
 let num1 = "";
 let num2 = "";
 let dispOperator = null;
@@ -14,6 +14,9 @@ const operate = (operator, list) => {
     if (!operation) {
         throw new Error("Unsupported operator");
     }
+    if (operator === "/" && list.find(number => number === 0) === 0) {
+        throw new Error("You cannot divide by 0 fucking idiot")
+    }
     let result = list.reduce(operation);
     DisplayOperator.textContent = '';
     secondDisplayNumber.textContent = '';
@@ -21,6 +24,8 @@ const operate = (operator, list) => {
     dispOperator = null;
     num2 = "";
     num1 = result.toString();
+    console.log(isAfterOperate)
+    isAfterOperate = true;
 }
 
 const container = document.querySelector('.container');
@@ -28,13 +33,19 @@ const display = document.getElementById("display");
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operators');
 const operateBtn = document.getElementById('operate');
-const clearButton = document.querySelector('clear');
+const clearButton = document.getElementById('clear');
 const firstDisplayNumber = document.getElementById('first-display-number');
 const DisplayOperator = document.getElementById('display-operator');
 const secondDisplayNumber = document.getElementById('second-display-number');
 
 numbers.forEach((button) => {
     button.addEventListener("click", (e) => {
+        if (isAfterOperate) {
+            clear(true);
+            isAfterOperate = false;
+            // num1 = e.target.dataset.value;
+            // console.log(isAfterOperate);
+        }
         if (dispOperator !== null) {
             num2 = num2.concat(e.target.dataset.value);
             secondDisplayNumber.textContent = num2;
@@ -62,3 +73,15 @@ operateBtn.addEventListener('click', (e) => {
     }
 })
 
+clearButton.addEventListener('click', () => {
+    clear(true);
+})
+
+const clear = (shouldClearNum1 = false) => {
+    if (shouldClearNum1) num1 = '';
+    num2 = '';
+    dispOperator = null;
+    DisplayOperator.textContent = '';
+    secondDisplayNumber.textContent = '';
+    firstDisplayNumber.textContent = '';
+}
